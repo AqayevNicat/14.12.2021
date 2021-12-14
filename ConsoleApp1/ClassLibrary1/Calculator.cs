36b4b8dc7f9839fcbf2b45e386ca4cc59e2237c4
AqayevNicat <nijatam@code.edu.az>
2021-12-14 06:24:13 +0400

    Library

---
+++ ConsoleApp1/ClassLibrary1/Calculator.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public void Toplama(int num1,int num2)
+        {
+            Console.WriteLine(num1+num2);
+        }
+        public void Cixma(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+        public void Vurma(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+        }
+        public void Bolme(int num1, int num2)
+        {
+            Console.WriteLine(num1 / num2);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator clc = new Calculator();
+            clc.Toplama(4, 2);
+            clc.Cixma(4, 2);
+            clc.Vurma(4, 2);
+            clc.Bolme(4, 2);
+        }
+    }
+}
